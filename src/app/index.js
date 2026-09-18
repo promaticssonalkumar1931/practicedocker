ac421b00e50b06d2507a687f7bc524accd2769e6
promaticssonal <promatics.sonalkumar@gmail.com>
2026-09-18 17:45:14 +0530

    changed the index file

--- src/app/index.js
+++ src/app/index.js
@@ -9,7 +9,7 @@ app.post('/register', authController.registerUser);
 app.post('/login', authController.loginUser);
 
 app.get('/',(req,res)=>{
-    res.send("App is running on the port 3000 you can check sonal")
+    res.send("App is running on the port 3000 you can check hello")
 })
 
 module.exports = app;
